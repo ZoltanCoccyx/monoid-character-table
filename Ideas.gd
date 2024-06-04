188ef79e519ac006fb26b1cf65f8a68ebb442fd0
JosephDayngerRuiz <114962122+JosephDayngerRuiz@users.noreply.github.com>
2024-06-04 14:49:36 -0600

    Create Ideas.gd
    An idea for generalized conjugacy classes to be treated as an object.

---
+++ Ideas.gd
@@ -0,0 +1,12 @@
+# Formally allow generalied conjugacy classes
+
+
+# DeclareCategory("IsGeneralizedConjugacyClass", IsObject);
+
+
+# DeclareOperation("GeneralizedConjugacyClass", [IsMonoid, IsObject]);
+# DeclareOperation("GeneralizedConjugacyClasses", [IsMonoid]);
+
+
+# DeclareAttribute("Representative", IsGeneralizedConjugacyClass);
+# DeclareAttribute("AmbientMonoid", IsGeneralizedConjugacyClass);
