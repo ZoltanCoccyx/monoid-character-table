23f3059784eef7da6ccfb39f187f70a5772bd484
JosephDayngerRuiz <114962122+JosephDayngerRuiz@users.noreply.github.com>
2024-06-09 21:50:30 -0600

    Update tempgdfile.gd
    Added the bare minimum of functions as attributes to appropriate objects to compute a Cartan matrix.

--- tempgdfile.gd
+++ tempgdfile.gd
@@ -6,3 +6,15 @@ DeclareAttribute("GeneralisedConjugacyClassesRepresentatives", IsSemigroup);
 DeclareAttribute("DClassBicharacter", IsGreensDClass);
 
 DeclareAttribute("RegularRepresentationBicharacter", IsSemigroup);
+
+DeclareAttribute("RClassBicharacterOfGroupHClass", IsGroupHClass);
+
+DeclareAttribute("RClassRadicalOfGroupHClass", IsGroupHClass);
+
+DeclareAttribute("RClassRadicalBicharacterOfGroupHClass", IsGroupHClass);
+
+DeclareAttribute("DiagonalOfCharacterTables",IsSemigroup);
+
+DeclareAttribute("MonoidCharacterTable",IsSemigroup);
+
+DeclareAttribute("MonoidCartanMatrix",IsSemigroup);
